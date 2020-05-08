1936571d8e4b295dfe3744f79d20bde051ef7538
Roihan Auliya Ulfattah <roihana@gmail.com>
2020-05-08 15:45:10 +0700

    test

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php 
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$action = $json->result->action;
+
+	/* switch ($text) {
+		case 'hi':
+			$speech = "Hi, Nice to meet you";
+			break;
+
+		case 'bye':
+			$speech = "Bye, good night";
+			break;
+
+		case 'anything':
+			$speech = "Yes, you can type anything here.";
+			break;
+		
+		default:
+			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			break;
+	} */
+	
+	if ($action == "sayHello")
+		$speech = "Hi, Nice to meet you..from Webhook";
+
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
+
+?>
