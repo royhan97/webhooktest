2aa03e3eeb67cfbedbdbfaec20a16125b970f9ae
Roihan Auliya Ulfattah <roihana@gmail.com>
2020-05-08 16:04:16 +0700

    test

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if($method == 'POST'){
 
 	$response = new \stdClass();
 	$response->speech = $speech;
-	$response->displayText = $speech;
+	$response->fulfillmentText = $speech;
 	$response->source = "webhook";
 	echo json_encode($response);
 }
